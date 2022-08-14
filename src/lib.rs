a52a824dfcaa66e11b52e4d0caabb1f47ac1d5e5
Carlos Macasaet <github@carlos-m.net>
2022-08-14 16:29:44 -0700

    Incomplete: 1856 - Maximum Subarray Min-Product
    From: https://leetcode.com/problems/maximum-subarray-min-product/

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,7 @@
 pub mod car_pooling;
 pub mod find_minimum_time_to_finish_all_jobs;
 pub mod lfu_cache;
+pub mod max_subarray_min_product;
 pub mod maximize_grid_happiness;
 pub mod search_in_rotated_sorted_array;
 pub mod time_based_key_value_store;
