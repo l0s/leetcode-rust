b691652413e21226eac251b514ecaf69a3c5c218
Carlos Macasaet <github@carlos-m.net>
2022-09-01 23:23:45 -0700

    637. Average of Levels in Binary Tree
    Performance improvement
    
    From: https://leetcode.com/problems/average-of-levels-in-binary-tree/

--- src/average_of_levels_in_binary_tree.rs
+++ src/average_of_levels_in_binary_tree.rs
@@ -23,37 +23,28 @@ impl TreeNode {
 }
 
 use std::cell::RefCell;
-use std::collections::btree_map::Entry::{Occupied, Vacant};
-use std::collections::BTreeMap;
 use std::rc::Rc;
 
 impl Solution {
     pub fn average_of_levels(root: Option<Rc<RefCell<TreeNode>>>) -> Vec<f64> {
-        let mut map = BTreeMap::default();
-        build_map(root, 0, &mut map);
+        let mut map = Vec::default();
+        build_map(&root, 0, &mut map);
         map.iter()
-            .map(|(_, value)| (value.1 as f64) / (value.0 as f64))
+            .map(|value| (value.1 as f64) / (value.0 as f64))
             .collect()
     }
 }
 
-fn build_map(
-    root: Option<Rc<RefCell<TreeNode>>>,
-    level: usize,
-    result: &mut BTreeMap<usize, (usize, i64)>,
-) {
+fn build_map(root: &Option<Rc<RefCell<TreeNode>>>, level: usize, result: &mut Vec<(usize, i64)>) {
     if let Some(root) = root {
-        match result.entry(level) {
-            Vacant(e) => {
-                e.insert((1, root.borrow().val as i64));
-            }
-            Occupied(mut e) => {
-                let mut bucket = e.get_mut();
-                bucket.0 += 1;
-                bucket.1 += root.borrow().val as i64;
-            }
-        };
-        build_map(root.borrow().left.clone(), level + 1, result);
-        build_map(root.borrow().right.clone(), level + 1, result);
+        let root = root.borrow();
+        if let Some((count, sum)) = result.get_mut(level) {
+            *count += 1;
+            *sum += root.val as i64;
+        } else {
+            result.push((1, root.val as i64));
+        }
+        build_map(&root.left, level + 1, result);
+        build_map(&root.right, level + 1, result);
     }
 }
