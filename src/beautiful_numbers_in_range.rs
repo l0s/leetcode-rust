e19852c356d612941dbbcc140c433ef2dddcd2f1
Carlos Macasaet <github@carlos-m.net>
2023-10-13 09:40:02 -0500

    2827. Number of Beautiful Integers in the Range
    https://leetcode.com/problems/number-of-beautiful-integers-in-the-range/

--- src/beautiful_numbers_in_range.rs
+++ src/beautiful_numbers_in_range.rs
@@ -5,46 +5,275 @@ pub struct Solution;
 
 impl Solution {
     pub fn number_of_beautiful_integers(low: i32, high: i32, k: i32) -> i32 {
-        eprintln!("-- number_of_beautiful_integers( {}, {}, {} )", low, high, k);
-        let high = high.min( 10i32.pow(8));
-        if let Some(first) = (low..=high.min(low + k)).find(|number| *number % k == 0) {
-            eprintln!("-- first: {}", first);
-            let mut result = 0;
-            let mut number = first;
-            while number <= high {
-                let digits = extract_digits(number);
-                if digits.len() % 2 != 0 {
-                    // jump to the next order of magnitude
-                    let new_min = 10i32.pow(digits.len() as u32);
-                    if let Some(next_min) = (new_min..=high.min(new_min + k))
-                        .find(|number| *number % k == 0) {
-                        number = next_min;
-                        continue;
-                    } else {
-                        break;
-                    }
-                }
-                let even_digits = digits.iter().filter(|digit| *digit % 2 == 0).count();
-                let odd_digits = digits.len() - even_digits;
-                if even_digits == odd_digits {
-                    result += 1;
-                }
-                number += k;
-            }
-            return result;
-        }
-        0
+        let low = low - 1;
+
+        let mut solver = Solver::from(k as usize);
+        let upper_count = solver.beautiful_numbers(&extract_digits(high),
+                                                   0,
+                                                   0,
+                                                   true,
+                                                   0,
+                                                   true,
+                                                   0);
+
+        let lower_count = if low == 0 {
+            1
+        } else {
+            let mut solver = Solver::from(k as usize);
+            solver.beautiful_numbers(&extract_digits(low),
+                                     0,
+                                     0,
+                                     true,
+                                     0,
+                                     true,
+                                     0)
+        };
+
+        (upper_count - lower_count) as i32
     }
 }
 
-fn extract_digits(mut number: i32) -> Vec<u8> {
+/// Inspired by:
+/// https://leetcode.com/problems/number-of-beautiful-integers-in-the-range/solutions/3933279/c-digit-dp-intuition-explanation-clear-implementation/
+struct Solver {
+    /// All beautiful numbers must be divisible by this number
+    divisor: usize,
+
+    /// storage for intermediate solutions
+    ///
+    /// The outer vector will have one entry for each digit of the input number. e.g. for "8191",
+    /// there will be four entries. There can be up to 10 digits. The middle vector will have one
+    /// entry for each possible number of even numbers (up to the number of digits). The inner-most
+    /// vector will have one entry for each possible number of odd numbers (up to the number of
+    /// digits).
+    state: Vec<Vec<Vec<Edge>>>,
+}
+
+impl From<usize> for Solver {
+    fn from(divisor: usize) -> Self {
+        assert_ne!(divisor, 0);
+        let state = vec![vec![vec![Edge::default(); 10]; 10]; 10];
+        Self {
+            divisor,
+            state,
+        }
+    }
+}
+
+/// Storage for intermediate solutions for both boundary and non-boundary values.
+#[derive(Default, Clone)]
+struct Edge {
+    /// intermediate values for boundary values
+    yes: LeadingZero,
+    /// intermediate values for non-boundary values
+    no: LeadingZero,
+}
+
+impl Edge {
+    /// Get the mutable storage for intermediate solutions
+    ///
+    /// Parameters:
+    /// - `is_edge` - whether the number is the upper bound
+    ///
+    /// Returns:
+    /// - mutable storage for intermediate solutions
+    fn leading_zero(&mut self, is_edge: bool) -> &mut LeadingZero {
+        if is_edge {
+            &mut self.yes
+        } else {
+            &mut self.no
+        }
+    }
+}
+
+/// Storage for intermediate solutions broken out by whether the number being considered has a
+/// leading zero.
+#[derive(Clone)]
+struct LeadingZero {
+    /// The number of beautiful numbers for each remainder if there is a leading zero (up to a size
+    /// of 21)
+    yes: Vec<Option<usize>>,
+    /// The number of beautiful numbers for each remainder if there is no leading zero (up to a size
+    /// of 21)
+    no: Vec<Option<usize>>,
+}
+
+impl LeadingZero {
+    /// Get the intermediate storage based on whether there is a leading zero
+    ///
+    /// Parameters:
+    /// - `has_leading_zero` - true if the string representation of the number has a leading zero
+    ///
+    /// Returns:
+    /// - mutable vector of optional counts
+    fn remainders(&mut self, has_leading_zero: bool) -> &mut Vec<Option<usize>> {
+        if has_leading_zero {
+            &mut self.yes
+        } else {
+            &mut self.no
+        }
+    }
+}
+
+impl Default for LeadingZero {
+    fn default() -> Self {
+        // k is between 1 and 20 (inclusive), so there are at most 21 distinct remainders
+        let yes = vec![None; 21];
+        let no = vec![None; 21];
+        Self {
+            yes,
+            no,
+        }
+    }
+}
+
+impl Solver {
+
+    /// Calculate the number of beautiful numbers between 0 and `digits`
+    ///
+    /// Parameters:
+    /// - digits - the upper bound, represented as individual base 10 digits, cannot exceed 10^9
+    /// - even_count - the number of even digits counted so far
+    /// - odd_count - the number of odd digits counted so far
+    /// - edge - true if the the number under consideration is the upper boundary
+    /// - index - the next index into `digits` to examine
+    /// - leading_zero - true if the string representation of the number has a leading zero
+    /// - remainder - the remainder after dividing the number by the divisor
+    fn beautiful_numbers(&mut self,
+                         digits: &[u8],
+                         even_count: usize,
+                         odd_count: usize,
+                         edge: bool,
+                         index: usize,
+                         leading_zero: bool,
+                         remainder: usize) -> usize {
+        if index == digits.len() {
+            return if even_count == odd_count && remainder % self.divisor == 0 {
+                1
+            } else {
+                0
+            };
+        }
+
+        if let Some(count) = &self.state[index][even_count][odd_count]
+            .leading_zero(edge)
+            .remainders(leading_zero)[remainder] {
+            return *count;
+        }
+
+        let mut result = 0;
+        if edge {
+            let digit = digits[index] as usize;
+            for digit in 0..digit {
+                let remainder = (digit * 10usize.pow((digits.len() - index - 1) as u32)
+                    + remainder)
+                    % self.divisor;
+                result +=
+                    self.beautiful_numbers_for_digit(digits,
+                                                     even_count,
+                                                     odd_count,
+                                                     index,
+                                                     leading_zero,
+                                                     digit,
+                                                     remainder);
+            }
+            let remainder =
+                ((digit * 10usize.pow((digits.len() - index - 1) as u32))
+                        + remainder)
+                    % self.divisor;
+            result += if digit % 2 == 0 {
+                self.beautiful_numbers(digits,
+                                       even_count + 1,
+                                       odd_count,
+                                       true,
+                                       index + 1,
+                                       false,
+                                       remainder)
+            } else {
+                self.beautiful_numbers(digits,
+                                       even_count,
+                                       odd_count + 1,
+                                       true,
+                                       index + 1,
+                                       false,
+                                       remainder)
+            };
+        } else {
+            for digit in 0..=9usize {
+                let remainder =
+                    ((digit * 10usize.pow((digits.len() - index - 1) as u32))
+                            + remainder)
+                        % self.divisor;
+                result +=
+                    self.beautiful_numbers_for_digit(digits,
+                                                     even_count,
+                                                     odd_count,
+                                                     index,
+                                                     leading_zero,
+                                                     digit,
+                                                     remainder);
+            }
+        }
+        *self.state.get_mut(index).unwrap()
+            .get_mut(even_count).unwrap()
+            .get_mut(odd_count).unwrap()
+            .leading_zero(edge)
+            .remainders(leading_zero)
+            .get_mut(remainder).unwrap() = Some(result);
+        result
+    }
+
+    fn beautiful_numbers_for_digit(&mut self,
+                                   digits: &[u8],
+                                   even_count: usize,
+                                   odd_count: usize,
+                                   index: usize,
+                                   leading_zero: bool,
+                                   digit: usize,
+                                   remainder: usize) -> usize {
+        if digit % 2 == 0 {
+            if leading_zero && digit == 0 {
+                self.beautiful_numbers(digits,
+                                       even_count,
+                                       odd_count,
+                                       false,
+                                       index + 1,
+                                       true,
+                                       remainder)
+            } else {
+                self.beautiful_numbers(digits,
+                                       even_count + 1,
+                                       odd_count,
+                                       false,
+                                       index + 1,
+                                       false,
+                                       remainder)
+            }
+        } else {
+            self.beautiful_numbers(digits,
+                                   even_count,
+                                   odd_count + 1,
+                                   false,
+                                   index + 1,
+                                   false,
+                                   remainder)
+        }
+    }
+}
+
+/// Convert a number into its base 10 digits
+///
+/// Parameters:
+/// - `number` - must be positive
+fn extract_digits(number: i32) -> Vec<u8> {
+    let mut number = number;
     let mut result = vec![];
     while number >= 10 {
         let digit = number % 10;
-        result.push(digit as u8);
+        result.insert(0, digit as u8);
         number /= 10;
     }
-    result.push(number as u8);
+    result.insert(0, number as u8);
     result
 }
 
